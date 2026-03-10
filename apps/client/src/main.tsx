d8259bffb6b46516b0cfdef33ab93202592285bc
kousei2071 <kousei2071@gmail.com>
2026-03-10 20:38:35 +0900

    ファイル整理

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GameProvider } from './context/GameContext';
-import './styles/global.css';
+import './styles/index.css';
 
 function setupMobileViewportStabilizer() {
   const root = document.documentElement;
